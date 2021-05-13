76541e3625bbb051ba34de8e1b47153371900e39
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-14 01:10:37 +0530

    Welcome to User Registration Programme

---
+++ src/com/company/bridegelabz/regexproblems/UserRegistrationCode.java
@@ -0,0 +1,10 @@
+package com.company.bridegelabz.regexproblems;
+
+public class UserRegistrationCode
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to User Registration Programme");
+
+    }
+}
