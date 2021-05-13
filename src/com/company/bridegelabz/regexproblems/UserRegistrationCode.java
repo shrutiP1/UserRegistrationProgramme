aa2ea6fa7657111b1082a5da39aa2f945879c04a
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-14 01:29:35 +0530

    ADD:Welcome

--- src/com/company/bridegelabz/regexproblems/UserRegistrationCode.java
+++ src/com/company/bridegelabz/regexproblems/UserRegistrationCode.java
@@ -12,6 +12,13 @@ public class UserRegistrationCode
         Matcher m=p.matcher(name);
         return m.matches();
     }
+    public static boolean isMatchesEmail(String text)
+    {
+        String regex="(^[a-zA-Z 1-9]+ *([.+-_]?[a-zA-Z 1-9]+)@([a-zA-Z 1-9][-]?)+[.][a-zA-Z ]{2,4}(.[a-zA-Z]{2})*$)";
+        Pattern p=Pattern.compile(regex);
+        Matcher m= p.matcher(text);
+        return m.matches();
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcome to User Registration Programme");
@@ -19,5 +26,7 @@ public class UserRegistrationCode
         System.out.println(isMatchesFirstName(firstName));
         String lastName="Shruti";
         System.out.println(isMatchesFirstName(lastName));
+        String email="shruti.xyz@bri.com.in";
+        System.out.println(isMatchesEmail(email));
     }
 }
