401a63c5774055e914e0f34ccd1186cd0c5c372b
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-14 01:19:48 +0530

    ADD:Validate First Name

--- src/com/company/bridegelabz/regexproblems/UserRegistrationCode.java
+++ src/com/company/bridegelabz/regexproblems/UserRegistrationCode.java
@@ -1,10 +1,22 @@
 package com.company.bridegelabz.regexproblems;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistrationCode
 {
+    public static boolean isMatchesFirstName(String name)
+    {
+        String regex1="^([A-Z][a-z]{3,30})$";
+        Pattern p=Pattern.compile(regex1);
+        Matcher m=p.matcher(name);
+        return m.matches();
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcome to User Registration Programme");
+        String firstName="Shruti";
+        System.out.println(isMatchesFirstName(firstName));
 
     }
 }
